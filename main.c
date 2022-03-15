962c9b7eeca4177901cfff2225d87bce54250177
Alvaro <ajsabini@gmail.com>
2022-03-15 10:08:49 -0700

    printf

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("%b\n", 98);
+    return (0);
+}
